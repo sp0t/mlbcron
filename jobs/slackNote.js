9b72b1e7cc5aa68438eec9dfaba44cca0a4bcd10
sp0t <chenyin1g992@gmail.com.com>
2024-03-25 16:53:59 -0700

    [fix]-database connection

--- jobs/slackNote.js
+++ jobs/slackNote.js
@@ -46,13 +46,10 @@ exports.slackNote = async() => {
     await client.connect();
     var res = await client.query(`SELECT * FROM odds_table WHERE state != '2' AND game_date = '${gamedate}' ORDER BY start_time ASC;`);
 
-    console.log(res.rows)
-
     if(res.rows != undefined) {
         for(var x = 0; x < res.rows.length; x++) {
             var startime = new Date(res.rows[x].start_time);
             if(getDiffernceDateWithMin(currentTime, startime) != -1 && getDiffernceDateWithMin(currentTime, startime) > 120) {
-                console.log(res.rows[x].game_id)
                 try {
                     var response = await axios.post('http://127.0.0.1:5000/getLineupStatus', {
                             gameid: res.rows[x].game_id,
@@ -61,15 +58,14 @@ exports.slackNote = async() => {
                     return;
                 }
 
-                console.log(response.data)
 
                 if(response.data != undefined) {
                     if (response.data.away != 0 || response.data.home != 0 ) {
                         var message = `${res.rows[x].away} @ ${res.rows[x].home}\n${res.rows[x].away} ${response.data.away == 1 ? checkmark: crossmark}\n${res.rows[x].home} ${response.data.home == 1 ? checkmark: crossmark}`;
-                        if(response.data.away != 0 || response.data.home != 0)
-                            var response = await client.query(`UPDATE odds_table SET state = '2' WHERE game_id = '${res.rows[x].game_id}';`);
+                        if(response.data.away != 0 && response.data.home != 0)
+                            var respond = await client.query(`UPDATE odds_table SET state = '2' WHERE game_id = '${res.rows[x].game_id}';`);
                         else
-                            var response = await client.query(`UPDATE odds_table SET state = '1' WHERE game_id = '${res.rows[x].game_id}';`);
+                            var respond = await client.query(`UPDATE odds_table SET state = '1' WHERE game_id = '${res.rows[x].game_id}';`);
                         
                         if(response.data.away != 0 && response.data.home != 0 || res.rows[x].state == 0)
                             await sendMessage(process.env.SLACK_CHANNEL_ID, message);
