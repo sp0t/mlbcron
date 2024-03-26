2093b9d224a0729a1e58639c81773c3a7777d69b
sp0t <chenyin1g992@gmail.com.com>
2024-03-26 05:36:43 -0700

    [fix]-database connection

--- jobs/test.js
+++ jobs/test.js
@@ -11,51 +11,56 @@ const autoBetSettle = async() => {
     })
 
     await client.connect();
+    var res_bettings = await client.query(`SELECT COUNT(betid), betdate, team1, team2, place FROM betting_table WHERE status = '0' AND regstate = '1' GROUP BY betdate, team1, team2, place;`);
     
-
-    try {
-        var response = await axios.post('http://127.0.0.1:5000/getWinStatus', {
-            gameid: '747815',
-        });
-    } catch (error) {
-        return;
-    }
-
-    var status = 0;
-    var team1 = 'Toronto Blue Jays';
-    var team2 = 'Pittsburgh Pirates';
-    var place = 'Pittsburgh Pirates';
-
-
-    if (response.data != undefined) {
-        if(response.data.away_score != undefined && response.data.home_score != undefined) {
-            if(response.data.away_score != 0 || response.data.home_score != 0) {
-                if (response.data.away_score > response.data.home_score) {
-                    if(team1 == place)
-                        status = 2;
-                    else
-                        status = 1;
-                } else if (response.data.away_score < response.data.home_score) {
-                    if(team1 == place)
-                        status = 1;
-                    else
-                        status = 2;
+    console.log(res_bettings.rows, res_bettings.rows.length)
+    if(res_bettings.rows != undefined && res_bettings.rows.length > 0) {
+        for(var x in res_bettings.rows) {
+            var gamedate = res_bettings.rows[x].betdate.replace(/-/g, '/');
+            var res = await client.query(`SELECT game_id FROM odds_table WHERE game_date = '${gamedate}' AND away = '${res_bettings.rows[x].team1}' AND home = '${res_bettings.rows[x].team2}';`);
+            if(res.rows != undefined && res.rows[0].game_id != undefined) {
+                try {
+                    var response = await axios.post('http://127.0.0.1:5000/getWinStatus', {
+                        gameid: res.rows[0].game_id,
+                    });
+                } catch (error) {
+                    return;
                 }
 
-                console.log('state', status)
-                
-                // try {
-                //     var response = await axios.post('http://127.0.0.1:5000/showbetting', {
-                //         gamedate: res_bettings.rows[x].betdate,
-                //         away: res_bettings.rows[x].team1,
-                //         home: res_bettings.rows[x].team2,
-                //         status: status,
-                //         place: res_bettings.rows[x].place
-                //     });
-                // } catch (error) {
-                //     return;
-                // }
+                var status = 0;
 
+                if (response.data != undefined) {
+                    if(response.data.away_score != undefined && response.data.home_score != undefined) {
+                        if(response.data.away_score != 0 || response.data.home_score != 0) {
+                            if (response.data.away_score > response.data.home_score) {
+                                if(res_bettings.rows[x].team1 == res_bettings.rows[x].place)
+                                    status = 2;
+                                else
+                                    status = 1;
+                            } else if (response.data.away_score < response.data.home_score) {
+                                if(res_bettings.rows[x].team1 == res_bettings.rows[x].place)
+                                    status = 1;
+                                else
+                                    status = 2;
+                            }
+
+                            console.log(res_bettings.rows[x].betdate, res_bettings.rows[x].team2, status, res_bettings.rows[x].place)
+                            
+                            try {
+                                var response = await axios.post('http://127.0.0.1:5000/showbetting', {
+                                    gamedate: res_bettings.rows[x].betdate,
+                                    away: res_bettings.rows[x].team1,
+                                    home: res_bettings.rows[x].team2,
+                                    status: status,
+                                    place: res_bettings.rows[x].place
+                                });
+                            } catch (error) {
+                                return;
+                            }
+
+                        }
+                    }
+                }
             }
         }
     }
