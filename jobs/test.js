15da72914ebe0bd8ac3c8f82b844b4c6bb870e4b
sp0t <chenyin1g992@gmail.com.com>
2024-03-26 05:23:31 -0700

    [fix]-database connection

--- jobs/test.js
+++ jobs/test.js
@@ -1,37 +1,7 @@
-require('dotenv').config()
 const axios = require("axios");
-const { dateToString, getDiffernceDateWithMin } = require('../function/time');
-const { WebClient } = require('@slack/web-api');
 const { Client } = require('pg');
 
-// Initialize a Slack client with your bot's token.
-const slackClient = new WebClient(process.env.SLACK_TOKEN);
-
-
-const sendMessage = async(channelId, messageText) => {
-    try {
-        const resp = await slackClient.conversations.join({
-            channel: channelId,
-        });
-
-        const response = await slackClient.chat.postMessage({
-            channel: channelId,
-            text: messageText, // Always include a 'text' parameter for fallback purposes.
-            // Add other parameters as needed, like 'blocks' for structured layout
-        });
-
-        console.log("Message sent successfully", response.ts);
-        return;
-    } catch (error) {
-        console.error("Error sending message:", error);
-    }
-}
-
-const slackNote = async() => {
-
-    const checkmark = ':white_check_mark:';
-    const crossmark = ':x:';
-
+const autoBetSettle = async() => {
     const client = new Client({
         user: 'postgres',
         host: 'localhost',
@@ -40,42 +10,53 @@ const slackNote = async() => {
         port: 5432,
     })
 
-    const currentTime = new Date();
-    const gamedate = dateToString(currentTime);
-
     await client.connect();
-    var res = await client.query(`SELECT * FROM odds_table WHERE state != '2' AND game_date = '${gamedate}' ORDER BY start_time ASC;`);
+    
 
-    if(res.rows != undefined) {
-        for(var x = 0; x < res.rows.length; x++) {
-            var startime = new Date(res.rows[x].start_time);
-            console.log(res.rows[x], startime)
-            if(getDiffernceDateWithMin(currentTime, startime) != -1 && getDiffernceDateWithMin(currentTime, startime) > 120) {
-                try {
-                    var response = await axios.post('http://127.0.0.1:5000/getLineupStatus', {
-                            gameid: res.rows[x].game_id,
-                        });
-                } catch (error) {
-                    return;
-                }
-                console.log(res.rows[x].game_id, response.data)
+    try {
+        var response = await axios.post('http://127.0.0.1:5000/getWinStatus', {
+            gameid: '747815',
+        });
+    } catch (error) {
+        return;
+    }
 
-                if(response.data != undefined) {
-                    if (response.data.away != 0 || response.data.home != 0 ) {
-                        var message = `${res.rows[x].away} @ ${res.rows[x].home}\n${res.rows[x].away} ${response.data.away == 1 ? checkmark: crossmark}\n${res.rows[x].home} ${response.data.home == 1 ? checkmark: crossmark}`;
-                        if(response.data.away != 0 && response.data.home != 0)
-                            var respond = await client.query(`UPDATE odds_table SET state = '2' WHERE game_id = '${res.rows[x].game_id}';`);
-                        else
-                            var respond = await client.query(`UPDATE odds_table SET state = '1' WHERE game_id = '${res.rows[x].game_id}';`);
-                        
-                        if(response.data.away != 0 && response.data.home != 0 || res.rows[x].state == 0)
-                            await sendMessage(process.env.SLACK_CHANNEL_ID, message);
-                        }
-                    }
+    var status = 0;
+
+    if (response.data != undefined) {
+        if(response.data.away_score != undefined && response.data.home_score != undefined) {
+            if(response.data.away_score != 0 || response.data.home_score != 0) {
+                if (response.data.away_score > response.data.home_score) {
+                    if(res_bettings.rows[x].team1 == res_bettings.rows[x].place)
+                        status = 2;
+                    else
+                        status = 1;
+                } else if (response.data.away_score < response.data.home_score) {
+                    if(res_bettings.rows[x].team1 == res_bettings.rows[x].place)
+                        status = 1;
+                    else
+                        status = 2;
                 }
+
+                console.log('state', status)
+                
+                // try {
+                //     var response = await axios.post('http://127.0.0.1:5000/showbetting', {
+                //         gamedate: res_bettings.rows[x].betdate,
+                //         away: res_bettings.rows[x].team1,
+                //         home: res_bettings.rows[x].team2,
+                //         status: status,
+                //         place: res_bettings.rows[x].place
+                //     });
+                // } catch (error) {
+                //     return;
+                // }
+
             }
         }
+    }
+    
     await client.end();
 }
 
-slackNote();
+autoBetSettle();
