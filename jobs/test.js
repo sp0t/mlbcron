005182b4823a9c8ac5f55dec63d3e0453b0e6278
sp0t <chenyin1g992@gmail.com.com>
2024-03-28 11:40:15 -0700

    [update]-auto bet settlement

--- jobs/test.js
+++ jobs/test.js
@@ -20,29 +20,30 @@ const update = async() => {
     console.log(res_lastday.rows)
 
     
-    // if (res_lastgame.rows[0].start_time != undefined && res_lastday.rows[0].game_date != undefined && res_lastgame.rows[0].game_id != undefined) {
-    //     const startime = new Date(res_lastgame.rows[0].start_time);
-    //     const gamedate = new Date(res_lastday.rows[0].game_date);   
-    //     const newdate = addOneDayToDate(addOneDayToDate(gamedate));
-    //     newdate.setUTCHours(0, 0, 0, 0); 
-    //     if(getDiffernceDateWithHour(newdate, currentTime) != -1 && getDiffernceDateWithHour(startime, currentTime) > 2) {
-    //         try {
-    //             var response = await axios.post('http://127.0.0.1:5000/getLastGameStatus', {
-    //                     gameid: res_lastgame.rows[0].game_id,
-    //                 });
-    //         } catch (error) {
-    //             return;
-    //         }
+    if (res_lastgame.rows[0].start_time != undefined && res_lastday.rows[0].game_date != undefined && res_lastgame.rows[0].game_id != undefined) {
+        const startime = new Date(res_lastgame.rows[0].start_time);
+        const gamedate = new Date(res_lastday.rows[0].update_date);   
+        const newdate = addOneDayToDate(addOneDayToDate(gamedate));
+        newdate.setUTCHours(0, 0, 0, 0); 
+        console.log(newdate)
+        // if(getDiffernceDateWithHour(newdate, currentTime) != -1 && getDiffernceDateWithHour(startime, currentTime) > 2) {
+        //     try {
+        //         var response = await axios.post('http://127.0.0.1:5000/getLastGameStatus', {
+        //                 gameid: res_lastgame.rows[0].game_id,
+        //             });
+        //     } catch (error) {
+        //         return;
+        //     }
     
-    //         if(response.data.state != undefined) {
-    //             if (response.data.state > 10) {
-    //                 // const response = await axios.post('http://127.0.0.1:5000/update_data', {
-    //                 // });
-    //                 console.log('update =====================')
-    //             }
-    //         }
-    //     }
-    // }
+        //     if(response.data.state != undefined) {
+        //         if (response.data.state > 10) {
+        //             // const response = await axios.post('http://127.0.0.1:5000/update_data', {
+        //             // });
+        //             console.log('update =====================')
+        //         }
+        //     }
+        // }
+    }
 }
 
 
