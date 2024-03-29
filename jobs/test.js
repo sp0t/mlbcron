b6e2fa26170af01052bcd080c926c355ebf6445b
sp0t <chenyin1g992@gmail.com.com>
2024-03-29 02:27:17 -0700

    [update]-auto bet settlement

--- jobs/test.js
+++ jobs/test.js
@@ -4,14 +4,16 @@ const { getDiffernceDateWithHour, addOneDayToDate } = require('../function/time'
 
 const update = async() => {
     try {
-        var response = await axios.post('http://127.0.0.1:5000/getWinStatus', {
-            gameid: '746089',
+        var response = await axios.post('http://127.0.0.1:5000/showbetting', {
+            gamedate: '2024-03-28',
+            away: 'New York Yankees',
+            home: 'Houston Astros',
+            status: 2,
+            place: 'New York Yankees'
         });
     } catch (error) {
         return;
     }
-
-    console.log(response)
 }
 
 
