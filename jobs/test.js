812d0bf1652e4ea64974cf13d5a7dfefcc9f2fd1
sp0t <chenyin1g992@gmail.com.com>
2024-03-29 02:21:01 -0700

    [update]-auto bet settlement

--- jobs/test.js
+++ jobs/test.js
@@ -5,7 +5,7 @@ const { getDiffernceDateWithHour, addOneDayToDate } = require('../function/time'
 const update = async() => {
     try {
         var response = await axios.post('http://127.0.0.1:5000/getWinStatus', {
-            gameid: '746418',
+            gameid: '746089',
         });
     } catch (error) {
         return;
