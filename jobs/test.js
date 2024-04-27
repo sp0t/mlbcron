3821eebec3c7afa88a8240f0ec1ba317f55ebf2f
sp0t <chenyin1g992@gmail.com.com>
2024-04-27 11:34:47 -0700

    [check] - update database

--- jobs/test.js
+++ jobs/test.js
@@ -4,6 +4,7 @@ const { getDiffernceDateWithHour, addOneDayToDate } = require('../function/time'
 const { dateToString, getDiffernceDateWithMin } = require('../function/time');
 
 const update = async() => {
+    console.log('1')
     const currentTime = new Date();
     const client = new Client({
         user: 'postgres',
@@ -12,11 +13,19 @@ const update = async() => {
         password: 'lucamlb123',
         port: 5432,
     })
+    console.log('2')
+
 
     await client.connect();
     var res_lastgame = await client.query(`SELECT * FROM odds_table WHERE start_time is NOT NULL ORDER BY start_time DESC LIMIT 1;`);
     var res_lastday = await client.query(`SELECT * FROM updates ORDER BY update_date DESC LIMIT 1;`);
     await client.end();
+
+    console.log('3')
+
+
+    console.log(res_lastgame)
+    console.log(res_lastday)
     
     if (res_lastgame.rows[0].start_time != undefined && res_lastday.rows[0].update_date != undefined && res_lastgame.rows[0].game_id != undefined) {
         const startime = new Date(res_lastgame.rows[0].start_time);
