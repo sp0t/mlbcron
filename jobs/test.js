3ae682ace294f0dcbf40cd350f2b17c009036dd2
sp0t <chenyin1g992@gmail.com.com>
2024-05-03 18:02:58 -0700

    [check] - update database

--- jobs/test.js
+++ jobs/test.js
@@ -1,10 +1,35 @@
+require('dotenv').config()
 const axios = require("axios");
-const { Client } = require('pg');
-const { getDiffernceDateWithHour, addOneDayToDate } = require('../function/time');
 const { dateToString, getDiffernceDateWithMin } = require('../function/time');
+const { WebClient } = require('@slack/web-api');
+const { Client } = require('pg');
+
+const slackClient = new WebClient(process.env.SLACK_TOKEN);
+
+
+const sendMessage = async(channelId, messageText) => {
+    try {
+        const resp = await slackClient.conversations.join({
+            channel: channelId,
+        });
+
+        const response = await slackClient.chat.postMessage({
+            channel: channelId,
+            text: messageText, // Always include a 'text' parameter for fallback purposes.
+            // Add other parameters as needed, like 'blocks' for structured layout
+        });
+
+        console.log("Message sent successfully", response.ts);
+        return;
+    } catch (error) {
+        console.error("Error sending message:", error);
+    }
+}
 
 const update = async() => {
-    const currentTime = new Date();
+    const checkmark = ':white_check_mark:';
+    const crossmark = ':x:';
+
     const client = new Client({
         user: 'postgres',
         host: 'localhost',
@@ -13,42 +38,41 @@ const update = async() => {
         port: 5432,
     })
 
+    const currentTime = new Date();
+    const gamedate = dateToString(currentTime);
 
     await client.connect();
-    var res_lastgame = await client.query(`SELECT * FROM odds_table WHERE start_time is NOT NULL ORDER BY start_time DESC LIMIT 1;`);
-    var res_lastday = await client.query(`SELECT * FROM updates ORDER BY update_date DESC LIMIT 1;`);
-    await client.end();
+    var res = await client.query(`SELECT * FROM odds_table WHERE state != '2' AND game_date = '${gamedate}' ORDER BY start_time ASC;`);
 
+    if(res.rows != undefined) {
+        for(var x = 0; x < res.rows.length; x++) {
+            var startime = new Date(res.rows[x].start_time);
+            console.log(res.rows[x].auto_bet);
+            // if(getDiffernceDateWithMin(currentTime, startime) != -1) {
+            //     try {
+            //         var response = await axios.post('http://127.0.0.1:5000/getLineupStatus', {
+            //                 gameid: res.rows[x].game_id,
+            //             });
+            //     } catch (error) {
+            //         return;
+            //     }
 
-    console.log(res_lastgame.rows[0].start_time)
-    console.log(res_lastday.rows[0].update_date)
-    
-    if (res_lastgame.rows[0].start_time != undefined && res_lastday.rows[0].update_date != undefined && res_lastgame.rows[0].game_id != undefined) {
-        const startime = new Date(res_lastgame.rows[0].start_time);
-        const gamedate = new Date(res_lastday.rows[0].update_date);   
-        const newdate = addOneDayToDate(gamedate);
-        newdate.setUTCHours(0, 0, 0, 0); 
-        if(getDiffernceDateWithHour(newdate, currentTime) != -1 && getDiffernceDateWithHour(startime, currentTime) > 4) {
-            console.log(res_lastgame.rows[0].game_id)
-            try {
-                var response = await axios.post('http://127.0.0.1:5000/getWinStatus', {
-                        gameid: res_lastgame.rows[0].game_id,
-                    });
-            } catch (error) {
-                return;
-            }
-
-            if (response.data != undefined) {
-                if(response.data.away_score != undefined && response.data.home_score != undefined) {
-                    if(parseInt(response.data.away_score) != 0 || parseInt(response.data.home_score) != 0) {
-                        console.log("start")
-                        const response = await axios.post('http://127.0.0.1:5000/update_data', {
-                        });
-                    }
-                }
+            //     if(response.data != undefined) {
+            //         if (response.data.away != 0 || response.data.home != 0 ) {
+            //             var message = `${res.rows[x].away} @ ${res.rows[x].home}\n${res.rows[x].away} ${response.data.away == 1 ? checkmark: crossmark}\n${res.rows[x].home} ${response.data.home == 1 ? checkmark: crossmark}`;
+            //             if(response.data.away != 0 && response.data.home != 0)
+            //                 var respond = await client.query(`UPDATE odds_table SET state = '2' WHERE game_id = '${res.rows[x].game_id}';`);
+            //             else
+            //                 var respond = await client.query(`UPDATE odds_table SET state = '1' WHERE game_id = '${res.rows[x].game_id}';`);
+                        
+            //             if(response.data.away != 0 && response.data.home != 0 || res.rows[x].state == 0)
+            //                 await sendMessage(process.env.SLACK_CHANNEL_ID, message);
+            //             }
+            //         }
+            //     }
             }
         }
-    }
+    await client.end();
 }
 
 
