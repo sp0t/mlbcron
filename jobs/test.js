501641574562d27cfdb244a106cc08a90578119c
sp0t <chenyin1g992@gmail.com.com>
2024-05-13 09:04:41 -0700

    [dev] - auto bet

--- jobs/test.js
+++ jobs/test.js
@@ -18,7 +18,8 @@ const update = async() => {
     const gamedate = dateToString(currentTime);
 
     await client.connect();
-    var res = await client.query(`SELECT * FROM odds_table WHERE auto_bet = '1' AND state = '2';`);
+    // var res = await client.query(`SELECT * FROM odds_table WHERE auto_bet = '1' AND state = '2';`);
+    var res = await client.query(`SELECT * FROM odds_table WHERE auto_bet = '1';`);
 
     if(res.rows != undefined) {
         for(var x = 0; x < res.rows.length; x++) {
