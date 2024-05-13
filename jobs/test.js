6aba95531718016a86984accb51166a8e1e41723
sp0t <chenyin1g992@gmail.com.com>
2024-05-13 09:20:40 -0700

    [dev] - auto bet

--- jobs/test.js
+++ jobs/test.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const axios = require("axios");
 const { dateToString, getDiffernceDateWithMin } = require('../function/time');
 const { Client } = require('pg');
-
+const { genToken } = require('../function/credential');
 
 const update = async() => {
 
@@ -18,6 +18,7 @@ const update = async() => {
     const gamedate = dateToString(currentTime);
 
     await client.connect();
+
     // var res = await client.query(`SELECT * FROM odds_table WHERE auto_bet = '1' AND state = '2';`);
     var res = await client.query(`SELECT * FROM odds_table WHERE auto_bet = '1';`);
 
@@ -31,12 +32,84 @@ const update = async() => {
                             gameid: res.rows[x].game_id,
                         });
                 } catch (error) {
+                    await client.end();
                     return;
                 }
 
                 
                 if(response.data != undefined) {
                     console.log(response.data)
+                    var token = genToken();
+                    var options = {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': token
+                        },
+                        params: {
+                            sportId: 3,
+                            leagueIds: 246,
+                        }
+                    };
+                    
+                    try {
+                        var [retodd, retfixture] = await Promise.all([
+                            axios.get("https://api.ps3838.com/v3/odds", options),
+                            axios.get("https://api.ps3838.com/v3/fixtures", options)
+                        ]);
+                    } catch (error) {
+                        console.log(error)
+                        await client.end();
+                        return;
+                    }
+
+                    if (retfixture.data.league == undefined) {
+                        await client.end();
+                        return;
+                    }
+            
+                    if (retfixture.data.league[0].events == undefined) {
+                        await client.end();
+                        return;
+                    }
+                    
+                    if (retfixture.data.league[0].events.length == 0) {
+                        await client.end();
+                        return;
+                    }
+                
+                    var events = retfixture.data.league[0].events;
+                
+                    if(retodd.data.leagues == undefined) {
+                        await client.end();
+                        return;
+                    }
+                    
+                    if(retodd.data.leagues[0].events == undefined) {
+                        await client.end();
+                        return;
+                    }
+                
+                    if(retodd.data.leagues[0].events.length == 0) {
+                        await client.end();
+                        return;
+                    }
+
+                    var games = retodd.data.leagues[0].events;
+
+                    for (var x in events) {
+                        for (var y in games) {
+                            if(games[y].id != undefined && games[y].id == events[x].id) {
+                                if(games[y].periods != undefined && games[y].periods[0].moneyline != undefined) {
+                                    if(games[y].periods[0].moneyline.away != undefined && games[y].periods[0].moneyline.home != undefined) {
+                                        console.log(events[x])
+                                        console.log(games[y])
+                                    }                                        
+                                }
+                            }
+                        }    
+                    }
+                } else {
+                    var respond = await client.query(`UPDATE odds_table SET auto_bet = '0' WHERE game_id = '${res.rows[x].game_id}';`);
                 }
             } else {
                 var respond = await client.query(`UPDATE odds_table SET auto_bet = '0' WHERE game_id = '${res.rows[x].game_id}';`);
