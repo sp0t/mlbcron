895f4c27b5e3485e858955031c4a22e4b97d980e
sp0t <chenyin1g992@gmail.com.com>
2024-05-14 19:40:41 -0700

    [dev] - auto bet

--- jobs/test.js
+++ jobs/test.js
@@ -184,7 +184,20 @@ const update = async() => {
                                                 try {
                                                     var awayres = await axios.post("https://api.ps3838.com/v2/bets/place", awayoption, headers);
                                                 } catch (error) {
-                                                    console.log(error)
+                                                    if (error.response) {
+                                                        // The request was made and the server responded with a status code
+                                                        // that falls out of the range of 2xx
+                                                        console.log(error.response.data);
+                                                        console.log(error.response.status);
+                                                        console.log(error.response.headers);
+                                                    } else if (error.request) {
+                                                        // The request was made but no response was received
+                                                        console.log(error.request);
+                                                    } else {
+                                                        // Something happened in setting up the request that triggered an Error
+                                                        console.log('Error', error.message);
+                                                    }
+                                                    console.log(error.config);
                                                     await client.end();
                                                     return;
                                                 }
@@ -216,7 +229,20 @@ const update = async() => {
                                                 try {
                                                     var homeres = await axios.post("https://api.ps3838.com/v2/bets/place", homeoption, headers);
                                                 } catch (error) {
-                                                    console.log(error)
+                                                    if (error.response) {
+                                                        // The request was made and the server responded with a status code
+                                                        // that falls out of the range of 2xx
+                                                        console.log(error.response.data);
+                                                        console.log(error.response.status);
+                                                        console.log(error.response.headers);
+                                                    } else if (error.request) {
+                                                        // The request was made but no response was received
+                                                        console.log(error.request);
+                                                    } else {
+                                                        // Something happened in setting up the request that triggered an Error
+                                                        console.log('Error', error.message);
+                                                    }
+                                                    console.log(error.config);
                                                     await client.end();
                                                     return;
                                                 }
