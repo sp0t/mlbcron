cbd5f9da08fa69f34e4bd71bb41ad2b436958a01
sp0t <chenyin1g992@gmail.com.com>
2024-05-20 18:07:50 -0700

    [dev] - auto bet robot

--- jobs/test.js
+++ jobs/test.js
@@ -166,6 +166,7 @@ const update = async() => {
                             if(games[y].id != undefined && games[y].id == events[x].id) {
                                 if(games[y].periods != undefined && games[y].periods[0].moneyline != undefined) {
                                     if(games[y].periods[0].moneyline.away != undefined && games[y].periods[0].moneyline.home != undefined) {
+                                        console.log(games[y].periods[0].moneyline.away, games[y].periods[0].moneyline.home)
                                         if(events[x].away == res.rows[i].away && events[x].home == res.rows[i].home && events[x].starts == res.rows[i].start_time) {
                                             console.log(games[y].periods[0].moneyline)
                                             if(away_odd != 0 && games[y].periods[0].moneyline.away >= away_odd) {
