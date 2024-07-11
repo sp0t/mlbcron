6c5712943b13109c9cb6917b85d9adbaf3cd26de
sp0t <chenyin1g992@gmail.com.com>
2024-07-10 18:06:43 -0700

    auto bet

--- jobs/saveOdds.js
+++ jobs/saveOdds.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
-const { genToken } = require('../function/credential');
-const { getTodayStartTime, getTodayAt2PM, getDiffernceDateWithMin, getDiffernceDateWithHour } = require('../function/time');
+const { dateToString, getDiffernceDateWithMin } = require('../function/time');
+const { decimalToAmerican, americanToDecimal } = require('../function/odds');
 const { Client } = require('pg');
+const { genToken, generateUUID } = require('../function/credential');
+const { randomUUID } = require('crypto');
 
 exports.saveOdds = async() => {
     var token = genToken();
