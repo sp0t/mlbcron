40f869924426451ceffe1b2a7b5d177cb8accf56
sp0t <Chenyin1g992@gmail.com>
2024-11-04 07:21:09 +0200

    [check] - database connection overflow

--- cron.js
+++ cron.js
@@ -6,11 +6,11 @@ const { slackNote } = require('./jobs/slackNote')
 const { update } = require('./jobs/update')
 const { sendOdds } = require('./jobs/sendOdds')
 
-const task1 = cron.schedule('*/20 * * * *', () => {
-    autoBetSettle();
-}, {
-  scheduled: true
-});
+// const task1 = cron.schedule('*/20 * * * *', () => {
+//     autoBetSettle();
+// }, {
+//   scheduled: true
+// });
 
 const task2 = cron.schedule('*/2 * * * *', () => {
     saveOdds();
@@ -18,36 +18,36 @@ const task2 = cron.schedule('*/2 * * * *', () => {
   scheduled: true
 });
 
-const task3 = cron.schedule('*/15 * * * * *', () => {
-  slackNote();
-}, {
-  scheduled: true
-});
+// const task3 = cron.schedule('*/15 * * * * *', () => {
+//   slackNote();
+// }, {
+//   scheduled: true
+// });
 
-const task4 = cron.schedule('*/30 * * * *', () => {
-    update();
-}, {
-  scheduled: true
-});
+// const task4 = cron.schedule('*/30 * * * *', () => {
+//     update();
+// }, {
+//   scheduled: true
+// });
 
-const task5 = cron.schedule('*/4 * * * *', () => {
-  autoBetPlace();
-}, {
-scheduled: true
-});
+// const task5 = cron.schedule('*/4 * * * *', () => {
+//   autoBetPlace();
+// }, {
+// scheduled: true
+// });
 
-const task6 = cron.schedule('*/4 * * * * *', () => {
-  console.log('task6.....started');
-  sendOdds();
-}, {
-scheduled: true
-});
+// const task6 = cron.schedule('*/4 * * * * *', () => {
+//   console.log('task6.....started');
+//   sendOdds();
+// }, {
+// scheduled: true
+// });
 
-task1.start();
-// task2.start();
-task3.start();
-task4.start();
-task5.start();
-task6.start();
+// task1.start();
+task2.start();
+// task3.start();
+// task4.start();
+// task5.start();
+// task6.start();
 
 console.log('Cron jobs have been initialized');
