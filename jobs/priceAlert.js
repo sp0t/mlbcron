7cf8cad3e9a8a228da84be19f683043681d72a6a
sp0t <Chenyin1g992@gmail.com>
2024-11-11 20:15:01 +0200

    [dev] - price alert

--- jobs/priceAlert.js
+++ jobs/priceAlert.js
@@ -131,17 +131,19 @@ exports.priceAlert = async() => {
                                     if(events[x].away >= price_request[k].awayprice && price_request[k].awaystate == '0') {
                                         var message = `${events[x].away} @ ${events[x].home}\n The price you requested on ${events[x].away} (${price_request[k].awayprice}) is now available ${price_request[k].bet == 1? 'and autobet will bet as requested': ''}`;
                                         await sendMessage(process.env.SLACK_PRICE_ID, message);
-                                        await client.query(`UPDATE price_table SET status = '1' WHERE game_id = '${price_request[k].game_id}';`);
+                                        await client.query(`UPDATE price_table SET awaystate = '1' WHERE game_id = '${price_request[k].game_id}';`);
                                         if(price_request[k].homestate == '1')
-                                            await client.query(`UPDATE price_table SET awaystate = '1' WHERE game_id = '${price_request[k].game_id}';`);
+                                            await client.query(`UPDATE price_table SET status = '1' WHERE game_id = '${price_request[k].game_id}';`);
                                     }
 
                                     if(events[x].home >= price_request[k].homeprice && price_request[k].homestate == '0') {
                                         var message = `${events[x].away} @ ${events[x].home}\n The price you requested on ${events[x].home} (${price_request[k].homeprice}) is now available ${price_request[k].bet == 1? 'and autobet will bet as requested': ''}`;
                                         await sendMessage(process.env.SLACK_PRICE_ID, message);
                                         await client.query(`UPDATE price_table SET homestate = '1' WHERE game_id = '${price_request[k].game_id}';`);
-                                        if(price_request[k].awaystate == '1' || events[x].away >= price_request[k].awayprice )
+                                        if(price_request[k].awaystate == '1' || events[x].away >= price_request[k].awayprice ) {
+                                            await client.query(`UPDATE price_table SET awaystate = '1' WHERE game_id = '${price_request[k].game_id}';`);
                                             await client.query(`UPDATE price_table SET status = '1' WHERE game_id = '${price_request[k].game_id}';`);
+                                        }
                                     }
                                 }
                         }
