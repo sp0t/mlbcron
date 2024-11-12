b9df05f28f21459ff4d9cad7490982a7abb4c219
sp0t <Chenyin1g992@gmail.com>
2024-11-12 02:27:49 +0200

    [dev] - price alert

--- jobs/test.js
+++ jobs/test.js
@@ -131,6 +131,7 @@ const priceAlert = async() => {
                                     if(games[y].periods[0].moneyline.away >= price_request[k].awayprice && price_request[k].awaystate == '0') {
                                         console.log(events[x].away);
                                         console.log(price_request[k].awayprice);
+                                        console.log(games[y].periods[0].moneyline.away);
                                         // var message = `${events[x].away} @ ${events[x].home}\n The price you requested on ${events[x].away} (${price_request[k].awayprice}) is now available ${price_request[k].bet == 1? 'and autobet will bet as requested': ''}`;
                                         // await sendMessage(process.env.SLACK_PRICE_ID, message);
                                         // await client.query(`UPDATE price_table SET awaystate = '1' WHERE game_id = '${price_request[k].game_id}';`);
@@ -141,6 +142,7 @@ const priceAlert = async() => {
                                     if(games[y].periods[0].moneyline.home >= price_request[k].homeprice && price_request[k].homestate == '0') {
                                         console.log(events[x].home);
                                         console.log(price_request[k].homeprice);
+                                        console.log(games[y].periods[0].moneyline.home);
                                         // var message = `${events[x].away} @ ${events[x].home}\n The price you requested on ${events[x].home} (${price_request[k].homeprice}) is now available ${price_request[k].bet == 1? 'and autobet will bet as requested': ''}`;
                                         // await sendMessage(process.env.SLACK_PRICE_ID, message);
                                         // await client.query(`UPDATE price_table SET homestate = '1' WHERE game_id = '${price_request[k].game_id}';`);
