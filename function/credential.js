2c207b81eaaa3320842f4f192d221323f42ec5a1
sp0t <Chenyin1g992@gmail.com>
2024-11-25 18:25:57 +0200

    [test] - price alert

--- function/credential.js
+++ function/credential.js
@@ -1,6 +1,6 @@
 
 exports.genToken = () => {
-	var str = "PWF380001B" + ':' + "B6jr^hVwq$@R";
+	var str = "PWF3800029" + ':' + "ccDD7788";
 	var token =  'Basic ' + Buffer.from(str, 'utf-8').toString('base64');
     return token;
 }
