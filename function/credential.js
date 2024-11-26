c6483c435083024adb06e84fec5ec5cc77ea12b2
sp0t <Chenyin1g992@gmail.com>
2024-11-26 14:23:16 +0200

    [test] - price alert

--- function/credential.js
+++ function/credential.js
@@ -1,6 +1,6 @@
 
 exports.genToken = () => {
-	var str = "PW7110000P" + ':' + "Password1!";
+	var str = "PWF3800029" + ':' + "ccDD77888";
 	var token =  'Basic ' + Buffer.from(str, 'utf-8').toString('base64');
     return token;
 }
